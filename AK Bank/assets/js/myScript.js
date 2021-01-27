4e4887a58868bab719a37df98b2dbc62679de034
Aynal369 <Aynal369@gmail.com>
2021-01-27 10:52:15 +0600

    Arranged code

--- AK Bank/assets/js/myScript.js
+++ AK Bank/assets/js/myScript.js
@@ -1,40 +1,37 @@
 // Login Button Even Handler
-const loginBtn = document.getElementById("login");
+const loginBtn = document.getElementById("loginBtn");
 loginBtn.addEventListener("click", function () {
   const loginArea = document.getElementById("loginArea");
   loginArea.style.display = "none";
   const transactionArea = document.getElementById("transactionArea");
   transactionArea.style.display = "block";
 });
-
 // Deposit Button Even Handler
-const depositBtn = document.getElementById("addDeposit");
+const depositBtn = document.getElementById("depositBtn");
 depositBtn.addEventListener("click", function () {
   const depositAmount = document.getElementById("depositAmount").value;
-  const depositNumbers = parseFloat(depositAmount);
-  updateSpanText("currentDeposit", depositNumbers);
-  updateSpanText("currentBalance", depositNumbers);
-
+  const depositNumber = parseFloat(depositAmount);
+  updateSpanText("currentDeposit", depositNumber);
+  updateSpanText("currentBalance", depositNumber);
   document.getElementById("depositAmount").value = "";
 });
 // Withdraw Button Even Handler
-const withdrawBtn = document.getElementById("addWithdraw");
+const withdrawBtn = document.getElementById("withdrawBtn");
 withdrawBtn.addEventListener("click", function () {
-  const withdrawNumbers = getInputNumber("withdrawAmount");
-  updateSpanText("currentWithdraw", withdrawNumbers);
-  updateSpanText("currentBalance", -1 * withdrawNumbers);
+  const withdrawNumber = getInputNumber("withdrawAmount");
+  updateSpanText("currentWithdraw", withdrawNumber);
+  updateSpanText("currentBalance", -1 * withdrawNumber);
   document.getElementById("withdrawAmount").value = "";
 });
-
+// My Function
 function getInputNumber(id) {
   const amount = document.getElementById(id).value;
-  const amountNumbers = parseFloat(amount);
-  return amountNumbers;
+  const amountNumber = parseFloat(amount);
+  return amountNumber;
 }
-
-function updateSpanText(id, depositNumbers) {
+function updateSpanText(id, depositNumber) {
   const current = document.getElementById(id).innerText;
-  const currentNumbers = parseFloat(current);
-  const totalAmount = depositNumbers + currentNumbers;
+  const currentNumber = parseFloat(current);
+  const totalAmount = depositNumber + currentNumber;
   document.getElementById(id).innerText = totalAmount;
 }
